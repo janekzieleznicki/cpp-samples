236a3097c2ff93cf01bf91c1b5f7850451463226
Jan Zieleźnicki <janek.zieleznicki@gmail.com>
2020-05-01 09:53:15 +0200

    gcc 10

--- simple20/Main.cpp
+++ simple20/Main.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
-#include <experimental/coroutine>
+#include <coroutine>
 
 
 struct generator {
   struct promise_type;
-  using handle = std::experimental::coroutine_handle<promise_type>;
+  using handle = std::coroutine_handle<promise_type>;
   struct promise_type {
     int current_value;
     auto get_return_object() { return generator{handle::from_promise(*this)}; }
-    auto initial_suspend() { return std::experimental::suspend_always{}; }
-    auto final_suspend() { return std::experimental::suspend_always{}; }
+    auto initial_suspend() { return std::suspend_always{}; }
+    auto final_suspend() { return std::suspend_always{}; }
     void unhandled_exception() { std::terminate(); }
     void return_void() {}
     auto yield_value(int value) {
       current_value = value;
-      return std::experimental::suspend_always{};
+      return std::suspend_always{};
     }
   };
   bool move_next() { return coro ? (coro.resume(), !coro.done()) : false; }
